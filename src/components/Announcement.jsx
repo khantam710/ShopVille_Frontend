c713c4ac1c3909ee2357702398239af7e2f6c1e2
khantam710 <134850822+khantam710@users.noreply.github.com>
2023-10-05 20:21:14 +0530

    add files

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+
+const moveLeftToRight = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+`;
+
+const Container = styled.div`
+  height: 30px;
+  background-color: #4c0a42;
+  color: white;
+  display: flex;
+  justify-content: center;
+  font-weight: 500;
+  align-items: center;
+`;
+
+const ScrollingTextWrapper = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  animation: ${moveLeftToRight} 10s linear infinite;
+`;
+
+const Announcement = () => {
+  return (
+    <div>
+      <Container>
+        <ScrollingTextWrapper>
+          Super Deal!! Free Shipping on Orders Over $50
+        </ScrollingTextWrapper>
+      </Container>
+    </div>
+  );
+};
+
+export default Announcement;
---
+++ src/requestMethods.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:4000/shopville/"
+const TOKEN = localStorage.getItem('access_token');
+// console.log(TOKEN)
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
+});
+
+export const userRequest= axios.create({
+    baseURL: BASE_URL,
+    headers:{access_token: `Bearer ${TOKEN}`}
+});

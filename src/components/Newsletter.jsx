0bf54dab12925b970d27837b5ed7bd07fa66dbd5
Tamanna Khan <134850822+khantam710@users.noreply.github.com>
2025-06-15 17:46:15 +0530

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,90 +1,108 @@
-// import React from 'react'
-// import styled from 'styled-components';
-// import SendIcon from '@mui/icons-material/Send';
-// import { mobile, tablet } from '../responsive';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import SendIcon from '@mui/icons-material/Send';
+import { mobile, tablet } from '../responsive';
 
-// const Container = styled.div`
-//     /* height: 60vh; */
-//     height: fit-content;
-//     background-color: #ffe8e8;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     flex-direction: column;
-//     padding: 40px 10px;
+const Container = styled.div`
+    height: fit-content;
+    background-color: #ffe8e8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 40px 10px;
+`;
 
+const Title = styled.h1`
+    font-size: 45px;
+    margin-bottom: 10px;
+    ${mobile({ fontSize: '30px' })}
+    ${tablet({ fontSize: '35px' })}
+`;
 
+const Desc = styled.h5`
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
+    ${mobile({ textAlign: 'center', fontSize: '20px' })}
+`;
 
-// `
-// const Title = styled.h1`
-//     font-size: 45px;
-//     margin-bottom: 10px;
-//     ${mobile({ fontSize: '30px' })}
-//     ${tablet({ fontSize: '35px' })}
+const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    ${mobile({ width: '80%' })}
+`;
 
+const Form = styled.form`
+    height: 100%;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+    width: 100%;
+    ${mobile({ width: '100%' })}
+`;
 
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding: 8px 0 0 20px;
+    font-size: 16px;
+    &:focus {
+        outline: none;
+    }
+`;
 
-// `
-// const Desc = styled.h5`
-//     font-size: 24px;
-//     font-weight: 300;
-//     margin-bottom: 20px;
-//     ${mobile({ textAlign: 'center', fontSize: '20px' })}
+const Button = styled.button`
+    flex: 1;
+    border: none;
+    background-color: #4c0a42;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
-// `
+const Newsletter = () => {
+    const [email, setEmail] = useState('');
 
-// const InputContainer = styled.div`
-//        width: 50%;
-//     height: 40px;
-//     display: flex;
-//     justify-content: center;
-//     ${mobile({ width: '80%' })}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!email) return;
 
-// `
-// const Form = styled.form`
-//     height: 100%;
-//     background-color: white;
-//     display: flex;
-//     justify-content: space-between;
-//     border: 1px solid lightgray;
-//     ${mobile({ width: '80%' })}
-// `
-// const Input = styled.textarea`
-//     border: none;
-//     flex: 8;
-//     padding: 8px 0 0 20px;
-//     &:focus{
-//         outline: none;
-//     }
-// `
-// const Button = styled.button`
-//     flex: 1;
-//     border: none;
-//     background-color: #4c0a42;
-//     color: white;
+        // Fire CleverTap event
+        if (window.clevertap) {
+            window.clevertap.event.push("Newsletter Signup", {
+                Email: email
+            });
+        }
 
-// `
+        alert('Thanks for subscribing!');
+        setEmail('');
+    };
 
-// const Newsletter = () => {
-//     return (
-//         <Container>
-//             <Title>NewsLetter</Title>
+    return (
+        <Container>
+            <Title>NewsLetter</Title>
+            <Desc>Get timely update from your favourite products.</Desc>
+            <InputContainer>
+                <Form onSubmit={handleSubmit}>
+                    <Input
+                        placeholder="Your Email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <Button type="submit">
+                        <SendIcon />
+                    </Button>
+                </Form>
+            </InputContainer>
+        </Container>
+    );
+};
 
-//             <Desc>
-//                 Get timely update from your favourite products.
-//             </Desc>
-
-//             <InputContainer>
-//                 <Form  action="mailto:sharmadivya2332@gmail.com" method="post" enctype="text/plain">
-//                     <Input placeholder='Your Email' name="message" />
-//                     <Button>
-//                         <SendIcon />
-//                     </Button>
-//                 </Form>
-
-//             </InputContainer>
-//         </Container>
-//     )
-// }
-
-// export default Newsletter
+export default Newsletter;

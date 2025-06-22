038f2ba7e70138d22e63b39448a44d8fcac9fe8f
Tamanna Khan <134850822+khantam710@users.noreply.github.com>
2025-06-22 15:53:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import SingleProduct from './pages/SingleProduct';
-import SingleProduct from './pages/PaymentSuccess';
+import PaymentSucces from './pages/PaymentSuccess';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
